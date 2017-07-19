25b368f8d041c55149d27affb1a91dd78256dcaf
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-07-19 16:13:33 -0400

    Adding anopther Component

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import Projects from './Components/Projects'
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      projects: [
+      projects: []
+    }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
         {
           title: 'Bisiness Website',
           category: 'Web Design'
@@ -20,13 +26,13 @@ class App extends Component {
           category: 'Web Development'
         }
       ]
-    }
+    });
   }
 
   render() {
     return (
       <div className="App">
-          My App
+          <AddProject />
           <Projects projects={this.state.projects}/>
       </div>
     );
