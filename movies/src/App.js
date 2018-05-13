206c5e1e7412331593712eb2ba43b4bca9c35aef
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-05-13 19:23:48 -0400

    Adding site title bar

--- movies/src/App.js
+++ movies/src/App.js
@@ -6,8 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-         
-      
+      <table class="titleBar">
+        <tbody>
+          <tr>
+            <td>
+              <img width="50" src="film-strip.svg"/>
+            </td>
+            <td width="8">
+            </td>
+            <td>
+              <h1>Movies DB Search</h1>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
 
       </div>
